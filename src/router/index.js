bb06812566252c93d614c6e18712faedc34eb618
Ahmet Egesel <ahmetegesel@hotmail.com>
2020-05-01 21:52:06 +0200

    Home page added and links to demo pages added to Home page.

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from '../views/Home';
 import ProductVModel from '../views/v-model-views/ProductVModel.vue';
 import ProductProduce from '../views/produce-views/ProductProduce.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
   {
     path: '/product-v-model',
     name: 'ProductVModel',
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="nav">
+    <router-link to="/product-produce">Product Produce Demo</router-link> |
+    <router-link to="/product-v-model">Product VModel Demo</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+};
+</script>
