d7a954f9963c8bcd7d194a608a9221c521a5199a
Ahmet Egesel <ahmetegesel@hotmail.com>
2020-05-02 21:22:52 +0200

    PField generic component added.

---
+++ src/components/produce-components/generic/base/PField.vue
@@ -0,0 +1,29 @@
+<template>
+  <fragment>
+    <slot></slot>
+  </fragment>
+</template>
+
+<script>
+import { Fragment } from 'vue-fragment';
+
+export default {
+  name: 'PField',
+  components: {
+    Fragment
+  },
+  props: {
+    field: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    produce() {
+      return {
+        [this.field]: this.$slots.default[0].componentInstance.produce()
+      };
+    }
+  }
+};
+</script>
